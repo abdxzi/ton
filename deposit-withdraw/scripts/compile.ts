ee5e7bfc95cf6265f5adaf3f043b3b63df2c9a9e
abdxzi <azeezonline2013@gmail.com>
2024-08-22 20:05:10 +0530

    Deposit, Withdraw Coins

---
+++ deposit-withdraw/scripts/compile.ts
@@ -0,0 +1,31 @@
+import * as fs from "fs";
+import process from "process";
+import { Cell } from "ton-core";
+import { compileFunc } from "@ton-community/func-js";
+
+const compileScript = async () => {
+    const compileResult = await compileFunc({
+        targets: ["./contracts/main.fc"],
+        sources: (x) => fs.readFileSync(x).toString("utf8")
+    });
+
+    if(compileResult.status == "error") {
+        console.log(`[!] OOOPS. COMPILATION ERROR! \n ${compileResult.message}`)
+        process.exit(1); 
+    }
+
+    console.log(`[*] Compilation Done.`)
+
+    const hexArtifact = "build/main.compiled.json";
+
+    fs.writeFileSync(
+        hexArtifact,
+        JSON.stringify({
+            hex: Cell.fromBoc(Buffer.from(compileResult.codeBoc, "base64"))[0].toBoc().toString("hex"),
+        })
+    );
+
+    console.log(`[*] Compiled code saved to: ${hexArtifact}`)
+}
+
+compileScript();
